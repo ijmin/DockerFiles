388487fb96b351439dda5f973db188df31959a91
ijmin <ijmin95@naver.com>
2022-01-06 17:49:39 +0900

    rpi multi arch

---
+++ RPI4b/docker_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ip=$(ifconfig enp3s0 | grep inet | awk '$1=="inet" {print $2}')
+# xhost +local:docker
+xhost +local:root
+# xhost +$ip
+
+docker run -it -d --name rpi \
+    --net=host \
+    --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e QT_X11_NO_MITSHM=1 \
+    -e DISPLAY=$DISPLAY \
+    -v $HOME/Docker/RPI4b/ros_ws:/home/noetic/catkin_ws/src \
+    ijmin/rpi:4b_ros_0
+    
+    # -e NVIDIA_VISIBLE_DEVICES=all \
