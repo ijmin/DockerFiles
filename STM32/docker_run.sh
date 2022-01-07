62f9ce6c2767965b99df0f4509f85cb61b873ad3
ijmin <ijmin95@naver.com>
2022-01-07 18:25:06 +0900

    stm version

--- STM32/docker_run.sh
+++ STM32/docker_run.sh
@@ -13,7 +13,7 @@ docker run -it -d --name stm \
     -e QT_X11_NO_MITSHM=1 \
     -e DISPLAY=$DISPLAY \
     -v $HOME/Docker/STM32/workspace:/home/stm \
-    ijmin/stm32cube:0.0
+    ijmin/stm32cube:ide_1.8.0
     
 # ttyACM
 # docker exec -it stm /opt/st/stm32cubeide_1.8.0/stm32cubeide
